ffa3da497ad50024db94b3328d5e4518b2026475
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-22 13:35:00 -0600

    Correccion 1.0 del ejercicio

--- Algebra.py
+++ Algebra.py
@@ -1,12 +1,14 @@
-a = int(input("Por favor ingrese el valor de: A"))
-b = int(input("Por favor ingrese el valor de: B"))
-c = int(input("Por favor ingrese el valor de: C"))
+print("\nBienvenido!")
+print("\nEn este programa podras dar una solucion rapida y sencilla a las Ecuaciones cuadráticas por fórmula general.")
+
+a =(float(input("\nPor favor ingrese el valor de A: ")))
+b =(float(input("Por favor ingrese el valor de B: ")))
+c =(float(input("Por favor ingrese el valor de C: ")))
 import math
-raiz = math.sqrt((b ^ 2)-4*a*c)
+raiz = math.sqrt((b ** 2)-4*a*c)
+x1= (-b + raiz)/(2*a)
 
-x1= (-b + raiz) / 2*a
+x2= (-b - raiz) / (2*a)
 
-x2= (-b - raiz) / 2*a
-
-print(f"x1={x1}")
-print(f"x2={x2}")
+print(f"x1= {x1:.2f}")
+print(f"x2= {x2:.2f}")
