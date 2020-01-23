04a5fb20f347bd489190c486655bce34ba091b1e
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-23 00:19:49 -0600

    Foto y tamaño de fuente.

--- PDF.py
+++ PDF.py
@@ -1,5 +1,8 @@
 from reportlab.pdfgen import canvas
 
 j = canvas.Canvas("Ejercicio PDF.pdf")
-j.drawString(100, 750, "Joshua Roman Vazquez Benitez")
+j.setFont('Helvetica', 20)
+j.drawString(180, 750, "Joshua Roman Vazquez Benitez",)
+j.drawImage("C:/Users/Administrador/Desktop/Josh.jpg", 70, 700, 70, 80, )
+j.showPage()
 j.save()
