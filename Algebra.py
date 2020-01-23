346600b9e37aeb5464c53cbb52553d15e6a23f58
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-22 22:25:57 -0600

    Correccion 2.0 de errores, error de valor 0 y valores identicos.

--- Algebra.py
+++ Algebra.py
@@ -1,14 +1,21 @@
+import sys
 print("\nBienvenido!")
 print("\nEn este programa podras dar una solucion rapida y sencilla a las Ecuaciones cuadráticas por fórmula general.")
-
-a =(float(input("\nPor favor ingrese el valor de A: ")))
-b =(float(input("Por favor ingrese el valor de B: ")))
-c =(float(input("Por favor ingrese el valor de C: ")))
+a = (float(input("\nPor favor ingrese el valor de A: ")))
+if a == 0:
+    print("Este no es un valor valido para A, compruebalo y vuelve a intentarlo :c.")
+    sys.exit()
+b = (float(input("Por favor ingrese el valor de B: ")))
+if b == 0:
+    print("Este no es un valor valido para B, compruebalo y vuelve a intentarlo :c.")
+    sys.exit()
+c = (float(input("Por favor ingrese el valor de C: ")))
+if a == b and a == c:
+    print("Error, no se pueden utilizar 3 valores identicos.")
+    sys.exit()
 import math
 raiz = math.sqrt((b ** 2)-4*a*c)
-x1= (-b + raiz)/(2*a)
-
-x2= (-b - raiz) / (2*a)
-
+x1 = (-b + raiz) / (2*a)
+x2 = (-b - raiz) / (2*a)
 print(f"x1= {x1:.2f}")
 print(f"x2= {x2:.2f}")
