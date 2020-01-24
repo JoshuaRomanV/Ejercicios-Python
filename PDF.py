b29c7f43012ceb4338c3562396d1b488060b76ff
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-01-24 01:07:12 -0600

    Se añadio una tabla con calificaciones al PDF.

--- PDF.py
+++ PDF.py
@@ -1,8 +1,32 @@
-from reportlab.pdfgen import canvas
+from reportlab.platypus import (Table, SimpleDocTemplate, TableStyle, Image)
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+I= Image("C:/Users/Administrador/Desktop/Josh.jpg", width= 100, height= 100)
 
-j = canvas.Canvas("Ejercicio PDF.pdf")
-j.setFont('Helvetica', 20)
-j.drawString(180, 750, "Joshua Roman Vazquez Benitez",)
-j.drawImage("C:/Users/Administrador/Desktop/Josh.jpg", 70, 700, 70, 80, )
-j.showPage()
-j.save()
+pdf= SimpleDocTemplate("Ejercicio .pdf", pagezise= letter)
+t = Table([
+    [I,'Joshua Roman Vazquez Benitez.',],
+    ['Materia', 'Calificacion'],
+    ['Geografia', 10],
+    ['Estructura Socioeconomica de Mexico', 10],
+    ['Produccion oral y escrita del ingles I', 'A'],
+    ['Atencion y servicio al cliente', 10],
+    ['Fundamentos de contabilidad industrial', 10],
+    ['Ciencias de la salud I', 10],
+    ['Psicologia I', 10],
+    ['Matematicas financieras I', 10],
+    ['Calculo  diferencial', 10,]
+])
+style = TableStyle ([
+    ('FONTSIZE',(0 ,0),(-1,0),25), # Tamaño de Letra
+    ('ALIGN', (0,0) ,(-1,-1),'CENTER'), # Alinear texto
+    ('BOX', (-1,-1),(-1,-1), 2, colors.black), # Color de borde
+    ('BOTTOMPADDING', (-1,0), (-1,0), 75), # Ajuste para letras maxusculas
+    ('BOTTOMPADDING', (0,0), (0,0), 25),
+    ('GRID', (0,1), (-1,-1), 2, colors.black),
+
+])
+t.setStyle(style)
+story = []
+story.append(t)
+pdf.build(story)
