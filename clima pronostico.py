56a1b68568f5063f1601e72e06dc03265d7fb541
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-25 22:55:43 -0600

    Clima en python

--- clima pronostico.py
+++ clima pronostico.py
@@ -2,11 +2,10 @@ import pyowm
 
 owm = pyowm.OWM('f92b732fb564fd9ae89096f6542dde43')
 
-location = owm.weather_at_coords(20.6, -100.3833)
+location = owm.weather_at_coords(20.6, -100.3833) #coordenadas de tu ubicacion.
 weather = location.get_weather()
 
 fc = owm.three_hours_forecast_at_coords(20.6, -100.3833)
 
 
-print(weather )
-print(fc)
+print(f"El clima actual es {weather}")
