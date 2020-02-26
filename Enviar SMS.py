ae7d88a8383082c656d86e6fa134c16d2b4b033c
Willowy7w7 <60124665+Willowy7w7@users.noreply.github.com>
2020-02-25 22:56:22 -0600

    Enviar SMS en Python.

--- Enviar SMS.py
+++ Enviar SMS.py
@@ -4,7 +4,6 @@ from twilio.rest import Client
 account_sid = "ACb61b99b4011303f21054eee6933acc1a"
 auth_token = "6065520f74f36a6cda80c7855406530f"
 client = Client(account_sid, auth_token)
-
 message = client.messages \
 	            .create(
 		body ="Lo hice :)    att- Joshua", # mensaje
